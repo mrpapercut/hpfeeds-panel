4c0f06507e9949a7622d49ac1ddcf59188433055
mrpapercut <mischa.rodermond@gmail.com>
2018-03-10 21:59:01 +0100

    Init repo

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractSass = new ExtractTextPlugin({
+    filename: 'css/[name].css'
+});
+
+module.exports = {
+    entry: ['./web/src/index.js', './web/css/index.scss'],
+    mode: 'production',
+    output: {
+        path: path.resolve(__dirname, "web", "dist"),
+        filename: "js/[name].js"
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+        }, {
+            test: /\.scss$/,
+            use: extractSass.extract({
+                use: [{
+                    loader: 'css-loader',
+                    options: {
+                        url: false
+                    }
+                }, {
+                    loader: 'sass-loader'
+                }]
+            })
+        }, {
+            test: /\.(woff2|ttf)$/,
+            use: {
+                loader: 'file-loader',
+                options: {
+                    name: 'fonts/[name].[ext]',
+                    outputPath: ''
+                }
+            }
+        }]
+    },
+    plugins: [
+        extractSass
+    ]
+};
