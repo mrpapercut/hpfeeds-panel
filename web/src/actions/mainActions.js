4c220a786729b4bb2cc7885bfa5dae38e1fb1e45
mrpapercut <mischa.rodermond@gmail.com>
2018-03-18 00:36:58 +0100

    tweaks to frontend

--- web/src/actions/mainActions.js
+++ web/src/actions/mainActions.js
@@ -2,14 +2,14 @@ export const GET_FEEDS = 'GET_FEEDS';
 export const getFeeds = () => {
     return function (dispatch) {
         return fetch('http://192.168.1.34:3000/search/')
-        .then(
-            response => response.json(),
-            error => console.log(error)
-        ).then(json =>
-            dispatch(receiveFeeds(json))
-        );
+            .then(
+                response => response.json(),
+                error => console.log(error)
+            ).then(json =>
+                dispatch(receiveFeeds(json))
+            );
     };
-}
+};
 
 export const RECEIVE_FEEDS = 'RECEIVE_FEEDS';
 export const receiveFeeds = feeds => {
@@ -18,20 +18,22 @@ export const receiveFeeds = feeds => {
         feeds: feeds.hits.hits,
         totalFeeds: feeds.hits.total
     };
-}
+};
 
 export const GET_CAPTURES = 'GET_CAPTURES';
 export const getCaptures = () => {
     return function (dispatch) {
         return fetch('http://192.168.1.34:3000/captures/')
-        .then(
-            response => response.json(),
-            error => console.log(error)
-        ).then(json => {
-            return dispatch(receiveCaptures(json))
-        });
+            .then(
+                response => response.json(),
+                error => console.log(error)
+            ).then(json => {
+                return dispatch(receiveCaptures(json));
+            }).catch(error => {
+                console.log(error);
+            });
     };
-}
+};
 
 export const RECEIVE_CAPTURES = 'RECEIVE_CAPTURES';
 export const receiveCaptures = captures => {
@@ -40,4 +42,4 @@ export const receiveCaptures = captures => {
         captures: captures.hits.hits,
         totalCaptures: captures.hits.total
     };
-}
+};
