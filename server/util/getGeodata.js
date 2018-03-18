13edaa5b4e71bd40787d05d4ff7422ac70106092
mrpapercut <mischa.rodermond@gmail.com>
2018-03-18 11:51:41 +0100

    Error handling in getGeodata.js

--- server/util/getGeodata.js
+++ server/util/getGeodata.js
@@ -1,16 +1,21 @@
 import request from 'request';
+import {logError} from './log';
 
 const getGeodata = (ipaddr, cb) => {
     const url = `http://getcitydetails.geobytes.com/GetCityDetails?fqcn=${ipaddr}`;
 
     request.get(url, (err, res, body) => {
-        const geodata = JSON.parse(body);
+        try {
+            const geodata = JSON.parse(body);
 
-        cb(err, {
-            'latitude': parseFloat(geodata.geobyteslatitude),
-            'longitude': parseFloat(geodata.geobyteslongitude),
-            'fqcn': `${geodata.geobytescity}, ${geodata.geobytescountry}`
-        });
+            cb(err, {
+                'latitude': parseFloat(geodata.geobyteslatitude),
+                'longitude': parseFloat(geodata.geobyteslongitude),
+                'fqcn': `${geodata.geobytescity}, ${geodata.geobytescountry}`
+            });
+        } catch (e) {
+            logError(e);
+        }
     });
 };
 
